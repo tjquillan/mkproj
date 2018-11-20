3b6597970919feb0d21f2c4d1e8a327dafe3d871
iboyperson <tjquillan@gmail.com>
2018-11-19 23:04:32 -0700

    improved setup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mkproj
 
-mkproj is a opinionated tool to generate base projects for a multitude of languages
+mkproj is an opinionated tool to generate base projects for a multitude of languages.
 
 ## Supported Languages
 * Python
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import codecs
 import os
 
 from setuptools import find_packages, setup
@@ -5,6 +6,10 @@ from setuptools import find_packages, setup
 # Taken from pipenv
 # See https://github.com/pypa/pipenv/blob/master/setup.py
 here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
 about = {}
 with open(os.path.join(here, "mkproj", "__version__.py")) as f:
     exec(f.read(), about)
@@ -12,19 +17,22 @@ with open(os.path.join(here, "mkproj", "__version__.py")) as f:
 setup(
     name='mkproj',
     version=about["__version__"],
-    packages=find_packages(exclude=["tasks", "tasks.*"]),
+    description='An opinionated tool to generate base projects for a multitude of languages.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/iboyperson/mkproj',
-    license='MIT',
     author='iboyperson',
     author_email='tjquillan@gmail.com',
-    description='An application to manage dotfiles efficiently with GNU Stow',
+    packages=find_packages(exclude=["tasks", "tasks.*"]),
     entry_points={
         'console_scripts': [
             'mkproj = mkproj:cli',
         ]
     },
     package_data={
-        "": "LICENSE",
+        "": ["LICENSE"],
+        "mkproj.templates": ["**/*.templ"]
     },
     include_package_data=True,
+    license='MIT',
 )
