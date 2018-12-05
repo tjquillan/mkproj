c5c602f58bd38e066fa2737f0e4ad14071b28198
iboyperson <tjquillan@gmail.com>
2018-12-04 22:01:07 -0700

    remove duplication of effort in printer

--- mkproj/printer.py
+++ mkproj/printer.py
@@ -5,30 +5,28 @@ import crayons
 from . import environment
 
 
-def print_info(string: str, indent: bool = False):
-    full_string = "{0} {1}".format(crayons.blue("=>"), string)
+INDENT_SIZE = 3
+
+
+def _print(prefix: str, string: str, indent: bool = False):
+    full_string = "{0} {1}".format(prefix, string)
     if indent:
-        full_string = "   {0}".format(full_string)
+        full_string = "{0}{1}".format((" " * INDENT_SIZE), full_string)
     echo(full_string)
+
+
+def print_info(string: str, indent: bool = False):
+    _print(crayons.blue("=>"), string, indent)
 
 
 def print_warning(string: str, indent: bool = False):
-    full_string = "{0} {1}".format(crayons.yellow("=>"), string)
-    if indent:
-        full_string = "   {0}".format(full_string)
-    echo(full_string)
+    _print(crayons.yellow("=>"), string, indent)
 
 
 def print_error(string: str, indent: bool = False):
-    full_string = "{0} {1}".format(crayons.red("=>"), string)
-    if indent:
-        full_string = "   {0}".format(full_string)
-    echo(full_string)
+    _print(crayons.red("=>"), string, indent)
 
 
 def print_verbose(string: str, indent: bool = False):
     if environment.verbosity:
-        full_string = "{0} {1}".format(crayons.magenta("=>"), string)
-        if indent:
-            full_string = "   {0}".format(full_string)
-        echo(full_string)
+        _print(crayons.magenta("=>"), string, indent)
