ca9f30946466d834a6bc2e43cee113d75c5afcd7
iboyperson <tjquillan@gmail.com>
2019-02-21 18:57:42 -0700

    add TaskIndex class for handling the tasks dict

--- mkproj/core.py
+++ mkproj/core.py
@@ -20,16 +20,63 @@ def depends(*deps):
     return wrapper
 
 
+class TaskIndex:
+    def __init__(self, project_name: str, project_path: Path, langs: list):
+        self._langs: list = langs
+
+        self._data: LockingDict = LockingDict(
+            {"project-name": project_name, "project-path": project_path}
+        )
+        self._tasks: Dict[str, BaseTask] = {}
+
+        self._index()
+
+    def __len__(self):
+        return len(self._tasks)
+
+    def __repr__(self):
+        return self._tasks.__repr__()
+
+    def _index(self):
+        self._tasks = dict(
+            (n.task_id(), n(self._data))
+            for n in BaseTask.__subclasses__()
+            if n.lang_id() in self._langs
+            and n.task_id() not in config.get_config("tasks", "skip")
+        )
+
+    def _task(self, task: str) -> BaseTask:
+        return self._tasks[task]
+
+    def lang_id(self, task: str) -> str:
+        return self._task(task).lang_id()
+
+    def task_ids(self) -> list:
+        return list(self._tasks.keys())
+
+    def task_id(self, task: str) -> str:
+        return self._task(task).task_id()
+
+    def depends(self, task: str) -> set:
+        return self._task(task).depends()
+
+    def config_defaults(self, task: str) -> Dict[str, dict]:
+        return self._task(task).config_defaults()
+
+    def run(self, task: str):
+        self._task(task).run()
+
+
 class TaskGraph:
-    def __init__(self, tasks: Dict[str, BaseTask]):
+    def __init__(self, tasks: TaskIndex):
         self._graph: networkx.DiGraph = networkx.DiGraph()
 
-        self._tasks = tasks
+        self._tasks: TaskIndex = tasks
 
         self._build_graph()
 
     @property
-    def tasks(self) -> Dict[str, BaseTask]:
+    def tasks(self) -> TaskIndex:
         return self._tasks
 
     @tasks.setter
@@ -38,12 +85,13 @@ class TaskGraph:
         self._build_graph()
 
     def _build_graph(self):
-        nodes: list = list(self._tasks[task].task_id() for task in self._tasks.keys())
+        nodes: list = self._tasks.task_ids()
         self._graph.add_nodes_from(nodes, success=False, error=False)
+
         edges: list = list(
-            (self._tasks[task].task_id(), dep)
-            for task in self._tasks.keys()
-            for dep in self._tasks[task].depends()
+            (task, dep)
+            for task in nodes
+            for dep in self._tasks.depends(task)
             if not set()
         )
         self._graph.add_edges_from(edges)
@@ -57,7 +105,7 @@ class TaskGraph:
                 and not len(list(self._graph.successors(node))) > 0
             ):
                 try:
-                    self._tasks[node].run()
+                    self._tasks.run(node)
                     self._graph.nodes[node]["success"] = True
                     edges: list = list(
                         (dep, node) for dep in self._graph.predecessors(node)
@@ -87,21 +135,11 @@ def create_project(project_name: str, state: State):
         spinner.print_error("Project already exists. Aborting...")
         sys.exit(1)
 
-    data: LockingDict = LockingDict(
-        {"project-name": project_name, "project-path": project_path}
-    )
-
     langs: list = ["generic"]
     if state.lang is not None:
         langs.append(state.lang)
 
-    tasks: dict = dict(
-        (n.task_id(), n(data))
-        for n in BaseTask.__subclasses__()
-        if n.lang_id() in langs
-        and n.task_id() not in config.get_config("tasks", "skip")
-    )
-
+    tasks: TaskIndex = TaskIndex(project_name, project_path, langs)
     graph: TaskGraph = TaskGraph(tasks)
 
     spinner.start()
